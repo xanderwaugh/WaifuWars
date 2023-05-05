5e7eb69ec745d523312c134cc0d9d7f518497e51
Xander Waugh <xanderwaugh531@gmail.com>
2023-05-05 00:24:18 -0400

    About Page

---
+++ src/pages/about.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+
+// Client
+import Head from "next/head";
+import Header from "~/components/Header";
+
+const AboutPage: NextPage = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-between gap-8 py-16">
+      <Head>
+        <title>Waifu Wars - About</title>
+      </Head>
+
+      <Header />
+
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="text-center text-4xl">About</div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
