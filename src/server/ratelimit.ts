e83f37d8327ef3d9bf5e0262b00f1fb67d2b3228
Xander Waugh <xanderwaugh531@gmail.com>
2023-05-26 16:57:54 -0400

    fixed ratelimit.

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -1,22 +1,20 @@
-import { env } from "~/env.mjs";
+// import { env } from "~/env.mjs";
 import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 import { type NextApiRequest } from "next";
 
 // * Rate Limiting
-const redis = new Redis({
-  url: env.UPSTASH_URL,
-  token: env.UPSTASH_TOKEN,
-});
+// const redis = new Redis({
+//   url: env.UPSTASH_URL,
+//   token: env.UPSTASH_TOKEN,
+// });
 
-// * Was 300ms
-const TIMEOUT_MS = 150;
+// * In ms - was 300
+// const TIMEOUT_MS = 150;
 
 const rateLimit = new Ratelimit({
-  redis,
-  timeout: TIMEOUT_MS, // * in ms
-  limiter: Ratelimit.slidingWindow(20, "10 s"),
-  // limiter: Ratelimit.fixedWindow(20, "1 s"),
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.fixedWindow(20, "10 s"),
 });
 
 const getFingerprint = (req: NextApiRequest) => {
@@ -35,7 +33,5 @@ export const ratelimit = async (req: NextApiRequest) => {
   await pending;
 
   console.timeEnd("Rate limit");
-  if (!success) return false;
-
-  return true;
+  return success;
 };
